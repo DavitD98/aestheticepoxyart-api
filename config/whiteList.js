226e8e15d04328d3592a0e6f10a3a509fd716231
David Peshtmajyan <david.peshtmajyan@mail.ru>
2023-06-29 20:06:50 +0400

    uploaded backend deploy

--- config/whiteList.js
+++ config/whiteList.js
@@ -1,7 +1,8 @@
 
 const WHITE_LIST = [
     `https://epoxyart.onrender.com`,
-    `http://localhost:3000`
+    `http://localhost:3000`,
+    `https://www.aestheticart.am`
 ]
 
 module.exports = WHITE_LIST
